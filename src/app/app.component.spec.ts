2fd0a25c0985184731cc78921d7281380b47a48a
Sebastiano Vierk <sebastiano.vierk@capgemini.com>
2022-01-11 19:02:26 +0100

    chore(setup): add eslint config and packages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,12 +13,14 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'angular-starter-kit'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('angular-starter-kit');
   });
 
@@ -26,6 +28,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
+
     expect(compiled.querySelector('.content span')?.textContent).toContain('angular-starter-kit app is running!');
   });
 });
